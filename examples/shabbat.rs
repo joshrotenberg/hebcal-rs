3ad2982f90be3f91f1839ad6ce81fadb83dad45b
josh rotenberg <joshrotenberg@gmail.com>
2020-10-12 14:38:40 -0700

    example for shabbat

--- examples/shabbat.rs
+++ examples/shabbat.rs
@@ -1,10 +1,19 @@
 use anyhow::Result;
-use hebcal::HebCal;
+use hebcal::{
+    shabbat::Transliteration,
+    HebCal,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let hebcal = HebCal::default();
-    let shabbat = hebcal.shabbat().send().await?;
+    let shabbat = hebcal
+        .shabbat()
+        .transliteration(Transliteration::Ashkenazic)
+        .zip("94706")
+        .havdalah(20u16)
+        .send()
+        .await?;
     println!("{:#?}", shabbat);
     Ok(())
 }
